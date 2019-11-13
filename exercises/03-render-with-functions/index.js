00cba7eb467f35b1f91dc295ea079e2398d8360f
Román Pozzo <rhpozzo@gmail.com>
2019-11-13 01:14:33 +0000

    updated excercise

--- exercises/03-render-with-functions/index.js
+++ exercises/03-render-with-functions/index.js
@@ -3,9 +3,9 @@ import React from "react"; //Main React.js library
 import ReactDOM from "react-dom"; //we use ReactDOM to render into the DOM
 
 // This function returns a string that will be rendered
-export const PrintHello = () => {
-	return <h1>Hello World</h1>;
+export const PrintHello = str => {
+	return <h1>{str}</h1>;
 };
 
 //              what           where
-ReactDOM.render(PrintHello(), document.querySelector("#myDiv"));
+ReactDOM.render(PrintHello("I Love React"), document.querySelector("#myDiv"));
